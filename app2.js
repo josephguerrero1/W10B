ced6d61aeaacbb6369dd835f5bafe250444c6e71
Joseph Guerrero <joseph.guerrero1@outlook.com>
2021-04-10 15:10:20 -0600

    Updated assignment

--- app2.js
+++ app2.js
@@ -1,18 +1,18 @@
 // Created 3 let variables with Cookies.get, which will fetch the cookies created in app.js.
-let red = Cookies.get("red", "red");
-let yellow = Cookies.get("yellow", "yellow");
-let green = Cookies.get("green", "green");
+let red = Cookies.get("red");
+let yellow = Cookies.get("yellow");
+let green = Cookies.get("green");
 
 // Created a function that will set the color of the div on the selection.html page.
 // if the cookie on the page has a name and value of red, the div will turn red.
 // if the cookie on the page has a name and value of yellow, the div will turn yellow.
 // if the cookie on the page has a name and value of green, the div will turn green.
-function setColor(getElementById) {
-    if (document.cookie = red) {
+function setColor() {
+    if (red) {
         document.getElementById("result").style.backgroundColor = "red";
-    } else if (document.cookie = yellow) {
+    } else if (yellow) {
         document.getElementById("result").style.backgroundColor = "yellow";
-    } else if (document.cookie = green) {
+    } else if (green) {
         document.getElementById("result").style.backgroundColor = "green";
     }
 }
@@ -22,7 +22,7 @@ setColor("result");
 
 // Created a function that will delete the cookies from the page.
 function restart() {
-    Cookies.remove("red", "red");
-    Cookies.remove("yellow", "yellow");
-    Cookies.remove("green", "green");
+    Cookies.remove("red");
+    Cookies.remove("yellow");
+    Cookies.remove("green");
 }
